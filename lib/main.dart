eb58befcd0acbb32d385bb1c65d8eedbf4c22758
Ronak <ronak.palenable@gmail.com>
2025-10-28 15:48:34 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'model/signup_screen.dart';
-  // Import the sign-up screen
+
 
 void main() {
   runApp(const MyApp());
@@ -14,13 +14,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Stock Management App',
+      title: 'Stock Management ',
       theme: ThemeData(
         primaryColor: const Color(0xFF0D47A1),  // Deep blue for stock theme
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green.shade700),  // Fixed: Use shade700 for non-nullable Color
         useMaterial3: true,
       ),
-      home: SignUpScreen(),  // Start with sign-up screen
+      home: SignUpScreen(),
     );
   }
 }
